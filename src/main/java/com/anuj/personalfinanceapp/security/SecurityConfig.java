ad386e585e12ee57a230ae4152c94e4f294dcb08
AnujKrSharma <anujkrsharma@hotmail.com>
2024-06-25 22:57:26 +0530

    Fixed issues For JWT login Now Application working properly

--- src/main/java/com/anuj/personalfinanceapp/security/SecurityConfig.java
+++ src/main/java/com/anuj/personalfinanceapp/security/SecurityConfig.java
@@ -3,6 +3,8 @@ package com.anuj.personalfinanceapp.security;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
@@ -25,12 +27,19 @@ public class SecurityConfig {
     }
 
     @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+
+
+    @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(csrf -> csrf.disable()) // Disable CSRF protection
                 .authorizeHttpRequests(authorizeRequests ->
                         authorizeRequests
-                                .requestMatchers("/api/auth/**", "/api-docs.yaml", "/swagger-ui/*").permitAll()
+                                .requestMatchers( "/api-docs.yaml", "/swagger-ui/*").permitAll()
+                                .requestMatchers("/api/auth/**").permitAll()
                                 .anyRequest().authenticated()
                 )
                 .sessionManagement(sessionManagement ->
