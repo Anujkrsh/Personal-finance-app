55c2d9bb06cfadf368d36b34f108f7978d5d9eab
AnujKrSharma <anujkrsharma@hotmail.com>
2024-07-08 10:41:57 +0530

    Added another Endpoint in Expense Api's

--- src/main/java/com/anuj/personalfinanceapp/Controller/ExpenseController.java
+++ src/main/java/com/anuj/personalfinanceapp/Controller/ExpenseController.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
@@ -20,7 +21,6 @@ public class ExpenseController {
 
     @Autowired
     private ExpenseService expenseService;
-
     @Autowired
     private JwtUtil jwtUtil;
     @Autowired
@@ -50,17 +50,60 @@ public class ExpenseController {
     }
 
     @GetMapping("/Fetch")
-    public ResponseEntity<?> FetchExpense(@RequestHeader("Authorization") String authToken) {
+    public ResponseEntity<?> fetchExpense(@RequestHeader("Authorization") String authToken) {
         try {
             long uuid = jwtUtil.extractUserId(authToken.substring(7));
             List<Expense> expense=expenseRepository.findByUserId(uuid);
-            if(!expense.isEmpty()) {
-                return ResponseEntity.ok(expense);
+            List<Expense> sortedExpenses = expense.stream()
+                    .sorted((e1, e2) -> e1.getId().compareTo(e2.getId()))
+                    .toList();
+            if(!sortedExpenses.isEmpty()) {
+                return ResponseEntity.ok(sortedExpenses);
             }else{
                 return ResponseEntity.badRequest().body("Expense Not Found");
             }
         }catch (Exception e) {
             return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
+
+    @PutMapping("/editExpense/{id}")
+    public ResponseEntity<?> editExpense(@RequestHeader("Authorization") String authToken,@PathVariable Long  id, @RequestBody ExpenseRequestDto expenseRequestDto) {
+        try{
+            validateExpense(expenseRequestDto);
+            long uuid = jwtUtil.extractUserId(authToken.substring(7));
+            List<Expense> expense=expenseRepository.findByUserId(uuid);
+            Expense expenseToEdit = expense.stream().filter(x->x.getId().equals(id)).findFirst().orElse(null);
+            LocalDateTime now = LocalDateTime.now();
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
+            String formattedDateTime = now.format(formatter);
+            if(expenseToEdit==null) {
+                return ResponseEntity.badRequest().body("Expense Not Found");
+            }else{
+                expenseToEdit= Expense.builder().amount(expenseRequestDto.getAmount()).date(formattedDateTime).
+                        item(expenseRequestDto.getItem())
+                        .description(expenseRequestDto.getDescription()).build();
+                expenseRepository.save(expenseToEdit);
+                return ResponseEntity.ok(expenseToEdit);
+            }
+        }catch (Exception e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
+
+    @DeleteMapping("/deleteExpense/{id}")
+    public ResponseEntity<?> deleteExpense(@RequestHeader("Authorization") String authToken,@PathVariable Long id) {
+        try{
+            expenseRepository.deleteById(id);
+        }catch (Exception e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+        return new ResponseEntity<>("Expense Deleted",HttpStatusCode.valueOf(200));
+    }
+
+    private void validateExpense(ExpenseRequestDto expense) {
+        if (expense==null||expense.getDescription()==null) {
+            throw new ResponseStatusException((HttpStatusCode.valueOf(401)),"Expense in Request Not Found");
         }
     }
 
